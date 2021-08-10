5cfb9762fd3803259ccdeb37d963445bc93e98a3
dami <damixyz@outlook.com>
2021-08-10 09:56:20 +0100

    Convert to data binding layout

--- app/src/main/java/com/damixyz/findcoffee/MainActivity.kt
+++ app/src/main/java/com/damixyz/findcoffee/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.damixyz.findcoffee
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.damixyz.findcoffee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil
+            .setContentView(this, R.layout.activity_main) as ActivityMainBinding
     }
 }
